8f554a8041beca0341115a0282bed6f9befa4e3f
Adam Williams <adam.williams@lifeomic.com>
2019-03-28 13:49:43 -0400

    Initialize from managed-integration-example@c5c8a91876a6cdc67a51afbb18aa40ab959f073d

---
+++ src/initializeContext.ts
@@ -0,0 +1,16 @@
+import {
+  IntegrationExecutionContext,
+  IntegrationInvocationEvent,
+} from "@jupiterone/jupiter-managed-integration-sdk";
+import ProviderClient from "./ProviderClient";
+import { ExampleExecutionContext } from "./types";
+
+export default function initializeContext(
+  context: IntegrationExecutionContext<IntegrationInvocationEvent>,
+): ExampleExecutionContext {
+  return {
+    ...context,
+    ...context.clients.getClients(),
+    provider: new ProviderClient(),
+  };
+}
