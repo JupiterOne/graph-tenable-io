b8153d3ebf45b5ac61081baf6fdd4fba64425421
Denis Arkhireev <den-uln@mail.ru>
2019-04-20 13:55:23 +0400

    Fixed client test

--- src/tenable/TenableClient.test.ts
+++ src/tenable/TenableClient.test.ts
@@ -113,7 +113,9 @@ describe("TenableClient fetch ok data", () => {
     expect(response.containers.length).not.toEqual(0);
     expect(response.reports.length).not.toEqual(0);
     expect(response.webAppVulnerabilities).not.toEqual({});
-    expect(response.containerVulnerabilities).not.toEqual({});
+    expect(response.findings).not.toEqual({});
+    expect(response.malwares).not.toEqual({});
+    expect(response.unwantedPrograms).not.toEqual({});
   });
 
   afterAll(() => {
