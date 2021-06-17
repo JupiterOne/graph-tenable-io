e3a800f7f9ef7e25507b1adb236bde4754a87ac4
Nick Dowmon <nick.dowmon@jupiterone.com>
2021-06-17 15:58:16 -0400

    Clean up timed-out export jobs

---
+++ test/recording.ts
@@ -0,0 +1,30 @@
+import {
+  Recording,
+  setupRecording,
+  SetupRecordingInput,
+} from "@jupiterone/integration-sdk-testing";
+import { TenableIntegrationConfig } from "../src/config";
+
+export { Recording };
+
+export function setupTenableRecording(input: SetupRecordingInput) {
+  return setupRecording({
+    redactedRequestHeaders: ["x-apikeys"],
+    ...input,
+  });
+}
+
+type MatchRequestsBy = Required<
+  SetupRecordingInput
+>["options"]["matchRequestsBy"];
+
+export function getTenableMatchRequestsBy(
+  config: TenableIntegrationConfig,
+  options?: MatchRequestsBy,
+): MatchRequestsBy {
+  return {
+    headers: false,
+    url: true,
+    ...options,
+  };
+}
