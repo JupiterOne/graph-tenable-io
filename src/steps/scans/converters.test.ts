e7780b672a57027c496e2a685baed876ae8e78b9
Nick Dowmon <nick.dowmon@jupiterone.com>
2021-06-22 15:46:56 -0400

    Add scan details

--- src/steps/scans/converters.test.ts
+++ src/steps/scans/converters.test.ts
@@ -1,5 +1,25 @@
-import { RecentScanSummary, ScanStatus, User } from '../../tenable/types';
-import { createScanEntity, createUserScanRelationship } from './converters';
+import { RelationshipDirection } from '@jupiterone/integration-sdk-core';
+
+import {
+  AssetExport,
+  RecentScanSummary,
+  ScanHostVulnerability,
+  ScanStatus,
+  ScanVulnerabilitySummary,
+  User,
+  VulnerabilityExport,
+} from '../../tenable/types';
+import {
+  createScanEntity,
+  createScanFindingRelationship,
+  createScanVulnerabilityRelationship,
+  createUserScanRelationship,
+  createVulnerabilityFindingEntity,
+  createVulnerabilityFindingRelationship,
+  getPriority,
+  getSeverity,
+  normalizeCVSS2Severity,
+} from './converters';
 
 describe('createScanEntity', () => {
   test('converts all expected properties', () => {
@@ -99,6 +119,386 @@ describe('convert user assessment relationships', () => {
       _key: 'tenable_user_2_owns_tenable_scan_12',
       _toEntityKey: 'tenable_scan_12',
       _type: 'tenable_user_owns_scan',
+    });
+  });
+});
+
+const scanSummary = {
+  id: 1234,
+  uuid: 'scan-uuid',
+} as RecentScanSummary;
+
+const vulnerabilitySummary = {
+  plugin_family: 'plugin-family',
+  plugin_name: 'plugin-name',
+  plugin_id: 112526,
+  severity: 199,
+  count: 291,
+} as ScanVulnerabilitySummary;
+
+const assetExport = {
+  id: 'asset-uuid',
+  fqdns: ['vpn.corporate.com'],
+  ipv4s: ['127.0.0.1'],
+  ipv6s: ['::1'],
+  mac_addresses: ['00:0a:95:9d:68:16'],
+} as AssetExport;
+
+const hostVulnerability = {
+  count: 1,
+  host_id: 2,
+  hostname: 'vpn.corporate.com',
+  plugin_family: 'plugin-family',
+  plugin_id: 3,
+  plugin_name: 'plugin-name',
+  severity: 3,
+} as ScanHostVulnerability;
+
+const vulnerabilityExport = {
+  asset: {
+    device_type: 'general-purpose',
+    hostname: '104.131.67.167',
+    uuid: '48cabb0b-f0fe-4db8-9a96-4fec60e4d4f4',
+    ipv4: '104.131.67.167',
+    last_unauthenticated_results: '2021-05-12T22:26:48Z',
+    operating_system: ['Linux Kernel 4.15 on Ubuntu 18.04 (bionic)'],
+    network_id: '00000000-0000-0000-0000-000000000000',
+    tracked: true,
+  },
+  output:
+    '\nThe following known CA certificates were part of the certificate\nchain sent by the remote host, but contain hashes that are considered\nto be weak.\n\n|-Subject             : O=Digital Signature Trust Co./CN=DST Root CA X3\n|-Signature Algorithm : SHA-1 With RSA Encryption\n|-Valid From          : Sep 30 21:12:19 2000 GMT\n|-Valid To            : Sep 30 14:01:15 2021 GMT\n',
+  plugin: {
+    bid: [11849, 33065],
+    cpe: ['cpe:/a:ietf:x.509_certificate', 'cpe:/a:ietf:md5'],
+    cve: ['CVE-2004-2761'],
+    exploit_available: true,
+    exploitability_ease: 'Exploits are available',
+    family: 'General',
+    family_id: 30,
+    has_patch: false,
+    id: 95631,
+    name: 'SSL Certificate Signed Using Weak Hashing Algorithm (Known CA)',
+    modification_date: '2019-11-26T00:00:00Z',
+    publication_date: '2016-12-08T00:00:00Z',
+    type: 'remote',
+    version: '1.12',
+    vuln_publication_date: '2004-08-18T00:00:00Z',
+    xrefs: [
+      { type: 'CERT', id: '836068' },
+      { type: 'CWE', id: '310' },
+    ],
+    vpr: {
+      score: 4.4,
+      drivers: {
+        age_of_vuln: { lower_bound: 731 },
+        exploit_code_maturity: 'PROOF_OF_CONCEPT',
+        cvss_impact_score_predicted: false,
+        cvss3_impact_score: 3.6,
+        threat_intensity_last28: 'VERY_LOW',
+        threat_sources_last28: ['No recorded events'],
+        product_coverage: 'LOW',
+      },
+      updated: '2020-12-30T05:20:01Z',
+    },
+  },
+  port: { port: 443, protocol: 'TCP', service: 'www' },
+  scan: {
+    completed_at: '2021-05-12T22:27:03.798Z',
+    schedule_uuid:
+      'template-cfdaeaa4-871d-cf00-62cc-b9f5488eb5dfe81ad4dd841d21cb',
+    started_at: '2021-05-12T21:12:24.210Z',
+    uuid: '92d56999-1a5a-4366-8742-6519013e8d72',
+  },
+  severity: 'info',
+  severity_id: 0,
+  severity_default_id: 0,
+  severity_modification_type: 'NONE',
+  first_found: '2021-05-12T22:27:03.798Z',
+  last_found: '2021-05-12T22:27:03.798Z',
+  state: 'OPEN',
+} as VulnerabilityExport;
+
+describe('getSeverity from numericSeverity', () => {
+  test('Informational for 0', () => {
+    expect(getSeverity(0)).toEqual('Informational');
+  });
+
+  test('Low for > 0 < 4', () => {
+    expect(getSeverity(0.1)).toEqual('Low');
+    expect(getSeverity(3.99)).toEqual('Low');
+  });
+
+  test('Medium for >= 4 < 7', () => {
+    expect(getSeverity(4)).toEqual('Medium');
+    expect(getSeverity(6.99)).toEqual('Medium');
+  });
+
+  test('High for >= 7 < 10', () => {
+    expect(getSeverity(7)).toEqual('High');
+    expect(getSeverity(9.99)).toEqual('High');
+  });
+
+  test('Critical for 10', () => {
+    expect(getSeverity(10)).toEqual('Critical');
+  });
+
+  test('for unknown severity', () => {
+    expect(getSeverity(11)).toEqual('Unknown');
+  });
+});
+
+describe('getPriority from numericPriority', () => {
+  test('Low for < 4', () => {
+    expect(getPriority(1)).toEqual('Low');
+  });
+
+  test('Medium for >= 4 and < 7', () => {
+    expect(getPriority(4)).toEqual('Medium');
+  });
+
+  test('High for >= 7 and < 9', () => {
+    expect(getPriority(7)).toEqual('High');
+  });
+
+  test('Critical for >= 9 and < 10', () => {
+    expect(getPriority(10)).toEqual('Critical');
+  });
+
+  test('for unknown priority', () => {
+    expect(getPriority(11)).toEqual('Unknown');
+  });
+});
+
+describe('normalizeCVSS2Severity', () => {
+  test('Low for < 4', () => {
+    expect(normalizeCVSS2Severity(0)).toEqual({
+      numericSeverity: 0,
+      severity: 'Low',
+    });
+    expect(normalizeCVSS2Severity(0.1)).toEqual({
+      numericSeverity: 0.1,
+      severity: 'Low',
+    });
+    expect(normalizeCVSS2Severity(3.99)).toEqual({
+      numericSeverity: 3.99,
+      severity: 'Low',
+    });
+  });
+
+  test('Medium for >= 4 < 7', () => {
+    expect(normalizeCVSS2Severity(4)).toEqual({
+      numericSeverity: 4,
+      severity: 'Medium',
+    });
+    expect(normalizeCVSS2Severity(6.99)).toEqual({
+      numericSeverity: 6.99,
+      severity: 'Medium',
+    });
+  });
+
+  test('High for >= 7 < 10', () => {
+    expect(normalizeCVSS2Severity(7)).toEqual({
+      numericSeverity: 7,
+      severity: 'High',
+    });
+    expect(normalizeCVSS2Severity(9.99)).toEqual({
+      numericSeverity: 9.99,
+      severity: 'High',
+    });
+    expect(normalizeCVSS2Severity(10)).toEqual({
+      numericSeverity: 10,
+      severity: 'High',
+    });
+  });
+
+  test('error for unknown severity', () => {
+    expect(normalizeCVSS2Severity(11)).toEqual({
+      numericSeverity: 11,
+      severity: undefined,
+    });
+  });
+});
+
+describe('createScanVulnerabilityRelationship', () => {
+  test('converts all expected properties', () => {
+    expect(
+      createScanVulnerabilityRelationship(scanSummary, vulnerabilitySummary),
+    ).toEqual({
+      _key: 'tenable_scan_1234_identified_tenable_vulnerability_112526',
+      _class: 'IDENTIFIED',
+      _type: 'tenable_scan_identified_vulnerability',
+      _mapping: {
+        relationshipDirection: RelationshipDirection.FORWARD,
+        sourceEntityKey: 'tenable_scan_1234',
+        targetFilterKeys: ['_key'],
+        targetEntity: {
+          _key: 'tenable_vulnerability_112526',
+          _type: 'tenable_vulnerability',
+          _class: 'Vulnerability',
+          pluginId: 112526,
+          pluginFamily: 'plugin-family',
+          pluginName: 'plugin-name',
+          numericSeverity: 199,
+          severity: 'Unknown',
+          displayName: 'plugin-name',
+        },
+      },
+      displayName: 'IDENTIFIED',
+      scanId: 1234,
+      scanUuid: 'scan-uuid',
+      count: 291,
+    });
+  });
+});
+
+describe('createVulnerabilityFindingRelationship', () => {
+  test('converts all expected properties', () => {
+    expect(
+      createVulnerabilityFindingRelationship({
+        scan: scanSummary,
+        assetUuid: assetExport.id,
+        vulnerability: hostVulnerability,
+      }),
+    ).toEqual({
+      _class: 'IS',
+      _key: 'tenable_vulnerability_finding_1234_3_2_tenable_vulnerability_3',
+      _type: 'tenable_vulnerability_finding_is_vulnerability',
+      _mapping: {
+        relationshipDirection: 'FORWARD',
+        sourceEntityKey: 'tenable_vulnerability_finding_1234_3_2',
+        targetEntity: {
+          _class: 'Vulnerability',
+          _key: 'tenable_vulnerability_3',
+          _type: 'tenable_vulnerability',
+          displayName: 'plugin-name',
+          pluginFamily: 'plugin-family',
+          pluginId: 3,
+          pluginName: 'plugin-name',
+          numericSeverity: 3,
+          severity: 'Low',
+        },
+        targetFilterKeys: ['_key'],
+      },
+      assetUuid: 'asset-uuid',
+      displayName: 'IS',
+      pluginId: 3,
+      scanId: 1234,
+      scanUuid: 'scan-uuid',
+    });
+  });
+});
+
+describe('createScanFindingRelationship', () => {
+  test('converts all expected properties', () => {
+    expect(
+      createScanFindingRelationship({
+        scan: scanSummary,
+        assetUuid: assetExport.id,
+        vulnerability: hostVulnerability,
+      }),
+    ).toEqual({
+      _class: 'IDENTIFIED',
+      _type: 'tenable_scan_identified_finding',
+      _key: 'tenable_scan_1234_tenable_vulnerability_finding_1234_3_2',
+      _fromEntityKey: 'tenable_scan_1234',
+      _toEntityKey: 'tenable_vulnerability_finding_1234_3_2',
+      assetUuid: 'asset-uuid',
+      displayName: 'IDENTIFIED',
+      pluginId: 3,
+      scanId: 1234,
+      scanUuid: 'scan-uuid',
+    });
+  });
+});
+
+describe('createVulnerabilityFindingEntity', () => {
+  test('converts all expected properties', () => {
+    const data = {
+      scan: scanSummary,
+      asset: assetExport,
+      assetUuid: assetExport.id,
+      vulnerability: hostVulnerability,
+      vulnerabilityExport,
+    };
+    const entity = createVulnerabilityFindingEntity(data);
+
+    expect(entity).toEqual({
+      _key: 'tenable_vulnerability_finding_1234_3_2',
+      _type: 'tenable_vulnerability_finding',
+      _class: 'Finding',
+      _rawData: [{ name: 'default', rawData: data }],
+      scanId: 1234,
+      scanUuid: 'scan-uuid',
+      assetUuid: 'asset-uuid',
+      hostId: 2,
+      hostname: 'vpn.corporate.com',
+      displayName: 'plugin-name',
+      pluginName: 'plugin-name',
+      pluginFamily: 'plugin-family',
+      pluginId: 3,
+      numericSeverity: 3,
+      severity: 'Low',
+      numericPriority: 4.4,
+      priority: 'Medium',
+      firstSeenOn: 1620858423798,
+      lastSeenOn: 1620858423798,
+      open: true,
+
+      // Something similar to
+      // https://bitbucket.org/lifeomic/jupiter-docs/pull-requests/217#Lguides/entity-relationship-mappings.mdT311
+      // may be necessary, see if placing all in targets will create
+      // relationships
+      targets: ['vpn.corporate.com', '127.0.0.1', '::1', '00:0a:95:9d:68:16'],
+    });
+  });
+
+  test('handles no vulnerability vpr data', () => {
+    const entity = createVulnerabilityFindingEntity({
+      scan: scanSummary,
+      asset: assetExport,
+      assetUuid: assetExport.id,
+      vulnerability: hostVulnerability,
+      vulnerabilityExport: {
+        ...vulnerabilityExport,
+        plugin: {
+          ...vulnerabilityExport.plugin,
+          vpr: undefined,
+        },
+      },
+    });
+    expect(entity).toMatchObject({
+      numericPriority: undefined,
+      priority: undefined,
+    });
+  });
+
+  test('handles no vulnerability export', () => {
+    const data = {
+      scan: scanSummary,
+      asset: assetExport,
+      assetUuid: assetExport.id,
+      vulnerability: hostVulnerability,
+      vulnerabilityExport: undefined,
+    };
+    const entity = createVulnerabilityFindingEntity(data);
+    expect(entity).toEqual({
+      _key: 'tenable_vulnerability_finding_1234_3_2',
+      _type: 'tenable_vulnerability_finding',
+      _class: 'Finding',
+      _rawData: [{ name: 'default', rawData: data }],
+      scanId: 1234,
+      scanUuid: 'scan-uuid',
+      assetUuid: 'asset-uuid',
+      hostId: 2,
+      hostname: 'vpn.corporate.com',
+      displayName: 'plugin-name',
+      pluginName: 'plugin-name',
+      pluginFamily: 'plugin-family',
+      pluginId: 3,
+      numericSeverity: 3,
+      severity: 'Low',
+      open: true,
+      targets: ['vpn.corporate.com', '127.0.0.1', '::1', '00:0a:95:9d:68:16'],
     });
   });
 });
--- src/steps/scans/converters.ts
+++ src/steps/scans/converters.ts
@@ -1,8 +1,25 @@
 import { Relationship } from '@jupiterone/integration-sdk-core';
 import { entities, relationships } from '../../constants';
-import { RecentScanSummary, User } from '../../tenable/types';
-import { generateEntityKey } from '../../utils/generateKey';
+import {
+  AssetExport,
+  AssetVulnerabilityRiskInfo,
+  RecentScanSummary,
+  ScanHostVulnerability,
+  ScanVulnerabilitySummary,
+  VulnerabilityExport,
+  User,
+} from '../../tenable/types';
+import {
+  generateEntityKey,
+  generateRelationshipKey,
+} from '../../utils/generateKey';
+import getTime from '../../utils/getTime';
 import getEpochTimeInMilliseconds from '../../utils/getEpochTimeInMilliseconds';
+
+import {
+  convertProperties,
+  RelationshipDirection,
+} from '@jupiterone/integration-sdk-core';
 
 export function createScanEntity(data: RecentScanSummary) {
   return {
@@ -34,7 +51,7 @@ export function createScanEntity(data: RecentScanSummary) {
   };
 }
 
-export function scanEntityKey(scanId: number): string {
+function scanEntityKey(scanId: number): string {
   return generateEntityKey(entities.SCAN._type, scanId);
 }
 
@@ -52,4 +69,270 @@ export function createUserScanRelationship(
     _toEntityKey: childKey,
   };
   return relationship;
+}
+
+// TODO: Move these into integration SDK and push out to other scanner
+// integrations
+export enum FindingSeverityPriority {
+  Informational = 'Informational',
+  Low = 'Low',
+  Medium = 'Medium',
+  High = 'High',
+  Critical = 'Critical',
+  Unknown = 'Unknown',
+}
+
+/**
+ * Converts Tenable Plugin Severity Ratings or "Risk Factor" to label. See
+ * https://community.tenable.com/s/article/Active-Plugins-Severity-vs-CVSS-v2-and-v3-scores
+ *
+ * Throws an `IntegrationError` when the Tenable severity number is not
+ * recognized.
+ */
+export function getSeverity(numericSeverity: number): FindingSeverityPriority {
+  if (numericSeverity === 0) {
+    return FindingSeverityPriority.Informational;
+  } else if (numericSeverity < 4) {
+    return FindingSeverityPriority.Low;
+  } else if (numericSeverity < 7) {
+    return FindingSeverityPriority.Medium;
+  } else if (numericSeverity < 10) {
+    return FindingSeverityPriority.High;
+  } else if (numericSeverity === 10) {
+    return FindingSeverityPriority.Critical;
+  } else {
+    return FindingSeverityPriority.Unknown;
+  }
+}
+
+/**
+ * Converts Tenable Priority Ratings to label. See
+ * https://docs.tenable.com/tenablesc/5_9/Content/RiskMetrics.htm#VPR
+ */
+export function getPriority(numericPriority: number): FindingSeverityPriority {
+  if (numericPriority < 4) {
+    return FindingSeverityPriority.Low;
+  } else if (numericPriority < 7) {
+    return FindingSeverityPriority.Medium;
+  } else if (numericPriority < 9) {
+    return FindingSeverityPriority.High;
+  } else if (numericPriority <= 10) {
+    return FindingSeverityPriority.Critical;
+  } else {
+    return FindingSeverityPriority.Unknown;
+  }
+}
+
+/**
+ * Converts NVD CVSS2 severity values to J1 normalized numeric values. See
+ * https://nvd.nist.gov/vuln-metrics/cvss.
+ *
+ * Throws an `IntegrationError` when the CVSS2 severity value is not recognized.
+ */
+export function normalizeCVSS2Severity(cvss2Severity: number | string): {
+  numericSeverity: number;
+  severity: FindingSeverityPriority | undefined;
+} {
+  const numericSeverity = Number(cvss2Severity);
+  let severity;
+  if (numericSeverity < 4) {
+    severity = FindingSeverityPriority.Low;
+  } else if (numericSeverity < 7) {
+    severity = FindingSeverityPriority.Medium;
+  } else if (numericSeverity <= 10) {
+    severity = FindingSeverityPriority.High;
+  }
+  return { numericSeverity, severity };
+}
+
+function createTenableVulnerabilityEntity(
+  vulnerability: ScanVulnerabilitySummary,
+) {
+  return {
+    _key: generateEntityKey(
+      entities.VULNERABILITY._type,
+      vulnerability.plugin_id,
+    ),
+    _type: entities.VULNERABILITY._type,
+    _class: entities.VULNERABILITY._class,
+    displayName: vulnerability.plugin_name,
+    pluginId: vulnerability.plugin_id,
+    pluginFamily: vulnerability.plugin_family,
+    pluginName: vulnerability.plugin_name,
+    numericSeverity: vulnerability.severity,
+    severity: getSeverity(vulnerability.severity),
+  };
+}
+
+export function createScanVulnerabilityRelationship(
+  scan: RecentScanSummary,
+  vulnerability: ScanVulnerabilitySummary,
+) {
+  const sourceEntityKey = generateEntityKey(entities.SCAN._type, scan.id);
+  const targetEntity = createTenableVulnerabilityEntity(vulnerability);
+
+  return {
+    _key: generateRelationshipKey(
+      sourceEntityKey,
+      relationships.SCAN_IDENTIFIED_VULNERABILITY._class,
+      targetEntity._key,
+    ),
+    _class: relationships.SCAN_IDENTIFIED_VULNERABILITY._class,
+    _type: relationships.SCAN_IDENTIFIED_VULNERABILITY._type,
+    _mapping: {
+      relationshipDirection: RelationshipDirection.FORWARD,
+      sourceEntityKey,
+      targetFilterKeys: ['_key'],
+      targetEntity: targetEntity as any,
+    },
+    displayName: 'IDENTIFIED',
+    scanId: scan.id,
+    scanUuid: scan.uuid,
+    count: vulnerability.count,
+  };
+}
+
+/**
+ * Create a relationship between a finding and the vulnerability that was found.
+ *
+ * The `vulnerability` is sufficient for building the relationship; the
+ * `Finding` key is derived from the scan/host/asset information.
+ */
+export function createVulnerabilityFindingRelationship({
+  scan,
+  assetUuid,
+  vulnerability,
+}: {
+  scan: RecentScanSummary;
+  assetUuid: string | undefined;
+  vulnerability: ScanHostVulnerability;
+}) {
+  const sourceEntityKey = vulnerabilityFindingEntityKey(scan, vulnerability);
+  const targetEntity = createTenableVulnerabilityEntity(vulnerability);
+
+  return {
+    _key: `${sourceEntityKey}_${targetEntity._key}`,
+    _type: relationships.FINDING_IS_VULNERABILITY._type,
+    _class: relationships.FINDING_IS_VULNERABILITY._class,
+    _mapping: {
+      relationshipDirection: RelationshipDirection.FORWARD,
+      sourceEntityKey,
+      targetFilterKeys: ['_key'],
+      targetEntity: targetEntity as any,
+    },
+    assetUuid,
+    displayName: 'IS',
+    pluginId: vulnerability.plugin_id,
+    scanId: scan.id,
+    scanUuid: scan.uuid,
+  };
+}
+
+/**
+ * Create a relationship between a finding and the scan that found it.
+ *
+ * The `vulnerability` is sufficient for building the relationship; the
+ * `Finding` key is derived from the scan/host/asset information.
+ */
+export function createScanFindingRelationship({
+  scan,
+  assetUuid,
+  vulnerability,
+}: {
+  scan: RecentScanSummary;
+  assetUuid: string | undefined;
+  vulnerability: ScanHostVulnerability;
+}) {
+  const findingKey = vulnerabilityFindingEntityKey(scan, vulnerability);
+  const scanKey = scanEntityKey(scan.id);
+  return {
+    _key: `${scanKey}_${findingKey}`,
+    _type: relationships.SCAN_IDENTIFIED_FINDING._type,
+    _class: relationships.SCAN_IDENTIFIED_FINDING._class,
+    _fromEntityKey: scanKey,
+    _toEntityKey: findingKey,
+    scanId: scan.id,
+    scanUuid: scan.uuid,
+    pluginId: vulnerability.plugin_id,
+    assetUuid,
+    displayName: 'IDENTIFIED',
+  };
+}
+
+export function createVulnerabilityFindingEntity(data: {
+  scan: RecentScanSummary;
+  asset: AssetExport | undefined;
+  assetUuid: string | undefined;
+  vulnerability: ScanHostVulnerability;
+  vulnerabilityExport?: VulnerabilityExport;
+}) {
+  const { scan, asset, assetUuid, vulnerability, vulnerabilityExport } = data;
+
+  const details = {};
+
+  if (vulnerabilityExport) {
+    const numericPriority =
+      vulnerabilityExport.plugin.vpr && vulnerabilityExport.plugin.vpr.score;
+    const priority = numericPriority && getPriority(numericPriority);
+    const cvss: AssetVulnerabilityRiskInfo = {
+      risk_factor: vulnerabilityExport.plugin.risk_factor,
+      cvss_vector: JSON.stringify(vulnerabilityExport.plugin.cvss_vector),
+      cvss_base_score: vulnerabilityExport.plugin.cvss_base_score?.toString(),
+      cvss_temporal_vector: vulnerabilityExport.plugin.cvss_temporal_vector,
+      cvss_temporal_score: vulnerabilityExport.plugin.cvss_temporal_score,
+      cvss3_vector: vulnerabilityExport.plugin.cvss3_vector,
+      cvss3_base_score: vulnerabilityExport.plugin.cvss3_base_score,
+      cvss3_temporal_vector: vulnerabilityExport.plugin.cvss3_temporal_vector,
+      cvss3_temporal_score: vulnerabilityExport.plugin.cvss_temporal_score,
+      stig_severity: vulnerabilityExport.plugin.stig_severity,
+    };
+
+    Object.assign(details, {
+      ...convertProperties(cvss),
+      description: vulnerabilityExport.plugin.description,
+      synopsis: vulnerabilityExport.plugin.synopsis,
+      solution: vulnerabilityExport.plugin.solution,
+      reference: vulnerabilityExport.plugin.see_also,
+      numericPriority,
+      priority,
+      firstSeenOn: getTime(vulnerabilityExport.first_found),
+      lastSeenOn: getTime(vulnerabilityExport.last_found),
+    });
+  }
+
+  return {
+    ...details,
+    _key: vulnerabilityFindingEntityKey(scan, vulnerability),
+    _type: entities.VULN_FINDING._type,
+    _class: entities.VULN_FINDING._class,
+    _rawData: [{ name: 'default', rawData: data }],
+    scanId: scan.id,
+    scanUuid: scan.uuid,
+    assetUuid,
+    hostId: vulnerability.host_id,
+    hostname: vulnerability.hostname,
+    displayName: vulnerability.plugin_name,
+    pluginName: vulnerability.plugin_name,
+    pluginFamily: vulnerability.plugin_family,
+    pluginId: vulnerability.plugin_id,
+    numericSeverity: vulnerability.severity,
+    severity: getSeverity(vulnerability.severity),
+    // Set open to true because we are only collecting findings from the latest assessment run
+    open: true,
+    targets:
+      asset &&
+      [asset.fqdns, asset.ipv4s, asset.ipv6s, asset.mac_addresses].reduce(
+        (a, e) => [...a, ...e],
+      ),
+  };
+}
+
+function vulnerabilityFindingEntityKey(
+  scan: RecentScanSummary,
+  vulnerability: ScanHostVulnerability,
+) {
+  return generateEntityKey(
+    entities.VULN_FINDING._type,
+    `${scan.id}_${vulnerability.plugin_id}_${vulnerability.host_id}`,
+  );
 }
